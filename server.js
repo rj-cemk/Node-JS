c8765e5ca71231570c49d22bcf83b2aa2e8e5dbf
Ravi Shankar <rj.cemk@gmail.com>
2017-09-01 12:28:31 +0530

    Starting with Basics

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Creating APIs')
+})
+ 
+app.get('/user', function (req, res) {
+    res.send('Creating APIs on URL Pattern')
+    console.log("GET Method")
+  })
+  
+console.log('Server started at port : 3000')
+app.listen(3000)
