6850d4701a3206c896989441227859389a6c7d5e
Ravi Shankar <rj.cemk@gmail.com>
2017-09-01 12:29:09 +0530

    Starting with Basics

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ app.get('/user', function (req, res) {
     res.send('Creating APIs on URL Pattern')
     console.log("GET Method")
   })
-  
 console.log('Server started at port : 3000')
 app.listen(3000)
